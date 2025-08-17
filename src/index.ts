77962bb2d8aef88b5acfa0cc455e2e1ee1812747
blaze308 <jaimepinkrah@gmail.com>
2025-08-17 22:26:07 +0000

    updated with cors

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ app.use(
     origin: process.env.ALLOWED_ORIGINS?.split(",") || [
       "http://localhost:3000",
       "http://localhost:3001",
+      "http://localhost:5173",
     ],
     credentials: true,
   })
